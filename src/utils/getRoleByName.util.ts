34cb048d121ae0bcb98c089e2eae161c3b59e1f5
Wesam Jabali <wesamjabali@icloud.com>
2023-02-25 05:24:49 -0600

    tolowercase

--- src/utils/getRoleByName.util.ts
+++ src/utils/getRoleByName.util.ts
@@ -2,7 +2,7 @@ import { client } from '@/services/discordClient.service';
 
 const getRoleByName = async (name: string) => {
     const guild = client.guilds.cache.get(process.env.GUILD_ID);
-    const role = guild.roles.cache.find((role) => role.name === name);
+    const role = guild.roles.cache.find((role) => role.name.toLowerCase() === name.toLowerCase());
     return role;
 };
 
